f380d1ce14e5b119164489473ba80bf75671d70d
angel_suarez <angelsuarezcuevas@gmail.com>
2021-09-21 23:41:36 -0500

    Se agregan nuevos vectores de datos y graficas

--- Script/script1.R
+++ Script/script1.R
@@ -7,3 +7,8 @@ dim(arbuthnot)
 names(arbuthnot)
 
 arbuthnot$boys
+ggplot(data = arbuthnot, aes(x = year, y = girls)) + geom_point()
+ggplot(data = arbuthnot, aes(x = year, y = boys)) + geom_point()
+
+arbuthnot <- arbuthnot %>% mutate(total = boys + girls)
+ggplot(data = arbuthnot, aes(x = year, y = total)) + geom_line()
