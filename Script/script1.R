acafc8b85098496193981d23b0a737a0afd84b5c
angel_suarez <angelsuarezcuevas@gmail.com>
2021-09-23 23:23:34 -0500

    Laboratorio terminado

--- Script/script1.R
+++ Script/script1.R
@@ -12,3 +12,21 @@ ggplot(data = arbuthnot, aes(x = year, y = boys)) + geom_point()
 
 arbuthnot <- arbuthnot %>% mutate(total = boys + girls)
 ggplot(data = arbuthnot, aes(x = year, y = total)) + geom_line()
+ggplot(data = arbuthnot, aes(x = year, y = total)) + geom_line() + geom_point()
+
+arbuthnot <- arbuthnot %>% mutate(more_boys = boys > girls)
+
+data(present)
+range(present$year) #Para obtener el rango de una vector de datos
+
+present <- present %>% mutate(total = boys + girls)
+present <- present %>%mutate(prop_boys = boys/total)
+ggplot(data = present, aes(x = year, y = prop_boys)) + geom_point() + geom_line()
+
+present <- present %>% mutate(more_boys = boys > girls)
+#Proporcion de nacimientos de niños niñas para cada año
+present <- present %>% mutate(prop_boy_girl = boys/girls)
+ggplot(data = present, aes(x = year, y = prop_boy_girl)) + geom_point() + geom_line()
+
+#Para ordenar los datos en orden desendente
+present %>% arrange(desc(total))
